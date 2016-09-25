a1f0f684d06d744688ca85f07db15cb9ae75782e
Matt Miller <mwm@fastmail.com>
2016-09-24 21:16:47 -0400

    Add row mirroring.
    We're going to eventually make a bunch of symmetric nonsense.
    This gets us the first bit along that road.
    
    They'll almost certainly become private before release.

--- lib/excon.ex
+++ lib/excon.ex
@@ -1,2 +1,13 @@
 defmodule Excon do
+
+  def mirror([], _, acc), do: acc |> Enum.reverse
+  def mirror([r|rows], :ltr, acc) do
+    mirror(rows, :ltr, [r |> Enum.concat(r |> Enum.reverse) | acc])
+  end
+  def mirror([r|rows], :rtl, acc) do
+    mirror(rows, :rtl, [(r|> Enum.reverse) |> Enum.concat(r) | acc])
+  end
+  def mirror(rows, :ttb, _), do: rows |> Enum.concat(Enum.reverse(rows))
+  def mirror(rows, :btt, _), do: Enum.reverse(rows) |> Enum.concat(rows)
+
 end
