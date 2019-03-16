f3c4389a688547c56739070326f523c9b0796c38
Matt Miller <mwm@fastmail.com>
2019-03-16 11:25:38 +0200

    Add foreground path

--- lib/excon.ex
+++ lib/excon.ex
@@ -131,21 +131,21 @@ defmodule Excon do
   end
 
   defp ident(hash, :svg, mag) do
-    <<cpo::integer-size(4), cpe::integer-size(4), bp::integer-size(2), c1::bitstring-size(6),
+    <<cpo::integer-size(4), cpe::integer-size(4), fp::integer-size(2), c1::bitstring-size(6),
       c2::bitstring-size(6), c3::bitstring-size(6), c4::bitstring-size(6),
-      bc::bitstring-size(6)>> = hash
+      fc::bitstring-size(6)>> = hash
 
     odds = elem(@palettes, cpo)
     evens = elem(@palettes, cpe)
-    bg = elem(@palettes, bp * 2)
+    fg = elem(@palettes, fp)
 
     """
     <svg width="#{8 * mag}" height="#{8 * mag}" version="1.1" xmlns="http://www.w3.org/2000/svg">
-    #{do_circle(4, 4, 3, bc, mag, bg)}
     #{do_circle(2, 2, 2, c1, mag, odds)}
     #{do_circle(2, 6, 2, c2, mag, evens)}
     #{do_circle(6, 6, 2, c3, mag, odds)}
     #{do_circle(6, 2, 2, c4, mag, evens)}
+    #{do_path(fc, mag, fg)}
     </svg>
     """
   end
@@ -153,7 +153,18 @@ defmodule Excon do
   defp do_circle(x, y, r, c, mag, pal),
     do: "<circle cx=\"#{x * mag}\" cy=\"#{y * mag}\" r=\"#{r * mag}\" #{svg_fill(c, pal)}/>"
 
-  defp svg_fill(<<w::integer-size(2), o::integer-size(4)>>, pal),
-    do:
-      "fill=\"rgba(#{pal |> Enum.fetch!(w) |> Tuple.to_list() |> Enum.join(",")},#{0.5 + o / 16}\""
+  defp do_path(
+         <<ys::integer-size(1), yf::integer-size(1), x1::integer-size(1), y1::integer-size(1),
+           x2::integer-size(1), y2::integer-size(1)>> = c,
+         mag,
+         pal
+       ),
+       do:
+         "<path d=\"M0,#{(ys * 4 + 4) * mag}  C#{x1 * 8 * mag},#{y1 * -2 * mag} #{x2 * -2 * mag},#{
+           y2 * 8 * mag
+         } #{8 * mag},#{(yf * 4 + 4) * mag}\" #{svg_fill(c, pal, 0.375)}/>"
+
+  defp svg_fill(<<w::integer-size(2), o::integer-size(4)>>, pal, opbase \\ 0.5) do
+    "fill=\"rgba(#{pal |> Enum.fetch!(w) |> Tuple.to_list() |> Enum.join(",")},#{opbase + o / 32}\""
+  end
 end
